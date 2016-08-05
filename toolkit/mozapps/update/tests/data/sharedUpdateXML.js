20962c9727b2859adcec96f7f487b82e3163f68d
Matt Howell <mhowell@mozilla.com>
2016-08-05 14:04:48 -0700

    Bug 1246972 - Always require the update working directory to be within the installation path; r?rstrong
    This required fixing a few chrome tests which broke on Mac because they were assuming updater-settings.ini would be at the same location as the executables.
    
    Also, this patch removes many dependencies on the current working directory from updater.cpp by changing it to use absolute paths instead. Otherwise this patch would have required adding yet more chdir() calls to avoid invalidating existing assumptions about what the current directory is.
    
    MozReview-Commit-ID: ASxfV4uVpmD
    
    --HG--
    extra : source : debce06e4d9a01d1ae9f7f8ffb9815dacc34cfaf

--- toolkit/mozapps/update/tests/data/sharedUpdateXML.js
+++ toolkit/mozapps/update/tests/data/sharedUpdateXML.js
@@ -35,11 +35,13 @@ const STATE_SUCCEEDED       = "succeeded";
 const STATE_DOWNLOAD_FAILED = "download-failed";
 const STATE_FAILED          = "failed";
 
-const STATE_FAILED_LOADSOURCE_ERROR_WRONG_SIZE = STATE_FAILED + ": 2";
-const STATE_FAILED_READ_ERROR                  = STATE_FAILED + ": 6";
-const STATE_FAILED_WRITE_ERROR                 = STATE_FAILED + ": 7";
-const STATE_FAILED_CHANNEL_MISMATCH_ERROR      = STATE_FAILED + ": 22";
-const STATE_FAILED_VERSION_DOWNGRADE_ERROR     = STATE_FAILED + ": 23";
+const STATE_FAILED_LOADSOURCE_ERROR_WRONG_SIZE      = STATE_FAILED + ": 2";
+const STATE_FAILED_READ_ERROR                       = STATE_FAILED + ": 6";
+const STATE_FAILED_WRITE_ERROR                      = STATE_FAILED + ": 7";
+const STATE_FAILED_CHANNEL_MISMATCH_ERROR           = STATE_FAILED + ": 22";
+const STATE_FAILED_VERSION_DOWNGRADE_ERROR          = STATE_FAILED + ": 23";
+const STATE_FAILED_INVALID_APPLYTO_DIR_STAGED_ERROR = STATE_FAILED + ": 72";
+const STATE_FAILED_INVALID_APPLYTO_DIR_ERROR        = STATE_FAILED + ": 74";
 
 /**
  * Constructs a string representing a remote update xml file.
