8f517b6f993e8a521ca53df4fcae9372ad9618a0
Doug Thayer <dothayer@mozilla.com>
2017-03-16 14:42:47 -0700

    Bug 893505 - Simplify the application update UI

---
+++ browser/base/content/test/appUpdate/browser_updatesBasicPrompt.js
@@ -0,0 +1,22 @@
+add_task(function* testBasicPrompt() {
+  pushPref("Bool", PREF_APP_UPDATE_STAGING_ENABLED, true);
+  let updateParams = "showPrompt=1&promptWaitTime=0";
+  gUseTestUpdater = true;
+
+  yield runUpdateTest(updateParams, 1, [
+    {
+      notificationId: "update-available",
+      button: "button",
+      beforeClick() {
+        checkWhatsNewLink("update-available-whats-new");
+      }
+    },
+    {
+      notificationId: "update-restart",
+      button: "secondarybutton",
+      cleanup() {
+        PanelUI.removeNotification(/.*/);
+      }
+    },
+  ]);
+});
