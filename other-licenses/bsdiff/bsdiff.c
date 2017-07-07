b30463431f76be3829849ee3377ee1554eb0b4e3
Robert Strong <robert.bugzilla@gmail.com>
2017-07-07 12:47:23 -0700

    Backed out changeset d47f445ac463 DONTBUILD

--- other-licenses/bsdiff/bsdiff.c
+++ other-licenses/bsdiff/bsdiff.c
@@ -23,7 +23,6 @@
 #include <string.h>
 #include <fcntl.h>
 #include <stdarg.h>
-#include "xz.h"
 #ifdef XP_WIN
 #include <io.h>
 #include <winsock2.h>
@@ -35,6 +34,26 @@
 
 #undef MIN
 #define MIN(x,y) (((x)<(y)) ? (x) : (y))
+
+/*---------------------------------------------------------------------------*/
+
+/* This variable lives in libbz2.  It's declared in bzlib_private.h, so we just
+ * declare it here to avoid including that entire header file.
+ */
+extern unsigned int BZ2_crc32Table[256];
+
+static unsigned int
+crc32(const unsigned char *buf, unsigned int len)
+{
+	unsigned int crc = 0xffffffffL;
+
+	const unsigned char *end = buf + len;
+	for (; buf != end; ++buf)
+		crc = (crc << 8) ^ BZ2_crc32Table[(crc >> 24) ^ *buf];
+
+	crc = ~crc;
+	return crc;
+}
 
 /*---------------------------------------------------------------------------*/
 
@@ -230,8 +249,7 @@ int main(int argc,char *argv[])
 		(close(fd)==-1))
 		reporterr(1,"%s\n",argv[1]);
 
-	xz_crc32_init();
-	scrc = xz_crc32(old, oldsize, 0);
+	scrc = crc32(old, oldsize);
 
 	if(((I=(int32_t*) malloc((oldsize+1)*sizeof(int32_t)))==NULL) ||
 		((V=(int32_t*) malloc((oldsize+1)*sizeof(int32_t)))==NULL))
