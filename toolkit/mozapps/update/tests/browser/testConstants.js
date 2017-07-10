edb4b76ac7f831a6dbbde60d3420ca7137dd8d80
Robert Strong <robert.bugzilla@gmail.com>
2017-07-09 18:18:03 -0700

    Backed out changeset 2ebfb9463c68 DONTBUILD

--- toolkit/mozapps/update/tests/browser/testConstants.js
+++ toolkit/mozapps/update/tests/browser/testConstants.js
@@ -1,6 +1,4 @@
-// MAR file URLs must be on 127.0.0.1 so that the update agent can access them;
-// it doesn't get the proxy magic that makes e.g. example.com work.
-const URL_HOST = "http://127.0.0.1:8888";
 const REL_PATH_DATA = "browser/toolkit/mozapps/update/tests/browser/";
+const URL_HOST = "http://example.com";
 const URL_PATH_UPDATE_XML = "/" + REL_PATH_DATA + "update.sjs";
 const URL_HTTP_UPDATE_SJS = URL_HOST + URL_PATH_UPDATE_XML;
