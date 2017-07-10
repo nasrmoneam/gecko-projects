209d2f2bd9b3af8b969ae90b97161823996cf2e5
Rail Aliiev <rail@mozilla.com>
2017-07-10 12:19:09 -0400

    No bug - Bump timeout

--- taskcluster/taskgraph/files_changed.py
+++ taskcluster/taskgraph/files_changed.py
@@ -28,7 +28,7 @@ def get_changed_files(repository, revision):
         logger.debug("Querying version control for metadata: %s", url)
 
         def get_automationrelevance():
-            response = requests.get(url, timeout=5)
+            response = requests.get(url, timeout=60)
             return response.json()
         contents = retry(get_automationrelevance, attempts=2, sleeptime=10)
 
