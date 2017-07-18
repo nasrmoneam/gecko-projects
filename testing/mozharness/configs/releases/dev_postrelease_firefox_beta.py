01a653ced5ba66946ee27f4766304ab370776a9c
Rail Aliiev <rail@mozilla.com>
2017-07-18 13:36:31 -0400

    More jamun changes

--- testing/mozharness/configs/releases/dev_postrelease_firefox_beta.py
+++ testing/mozharness/configs/releases/dev_postrelease_firefox_beta.py
@@ -1,17 +1,17 @@
 config = {
-    # date is used for staging mozilla-beta
-    "log_name": "bump_date",
+    # jamun is used for staging mozilla-beta
+    "log_name": "bump_jamun",
     "version_files": [{"file": "browser/config/version_display.txt"}],
     "repo": {
-        # date is used for staging mozilla-beta
-        "repo": "https://hg.mozilla.org/projects/date",
+        # jamun is used for staging mozilla-beta
+        "repo": "https://hg.mozilla.org/projects/jamun",
         "branch": "default",
-        "dest": "date",
+        "dest": "jamun",
         "vcs": "hg",
         "clone_upstream_url": "https://hg.mozilla.org/mozilla-unified",
     },
-    # date is used for staging mozilla-beta
-    "push_dest": "ssh://hg.mozilla.org/projects/date",
+    # jamun is used for staging mozilla-beta
+    "push_dest": "ssh://hg.mozilla.org/projects/jamun",
     "ignore_no_changes": True,
     "ssh_user": "ffxbld",
     "ssh_key": "~/.ssh/ffxbld_rsa",
--- testing/mozharness/configs/releases/dev_updates_firefox_beta.py
+++ testing/mozharness/configs/releases/dev_updates_firefox_beta.py
@@ -11,8 +11,8 @@ config = {
     "vcs_share_base": "/builds/hg-shared",
     # TODO: use real repo
     "push_dest": "ssh://hg.mozilla.org/users/raliiev_mozilla.com/tools",
-    # date repo used for staging beta
-    "shipped-locales-url": "https://hg.mozilla.org/projects/date/raw-file/{revision}/browser/locales/shipped-locales",
+    # jamun repo used for staging beta
+    "shipped-locales-url": "https://hg.mozilla.org/projects/jamun/raw-file/{revision}/browser/locales/shipped-locales",
     "ignore_no_changes": True,
     "ssh_user": "ffxbld",
     "ssh_key": "~/.ssh/ffxbld_rsa",
