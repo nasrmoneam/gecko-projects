98d17916ed7423029f77a489589fe25e2910fcff
Robert Strong <robert.bugzilla@gmail.com>
2017-10-09 10:46:31 -0700

    Backed out changeset 7dac8fa6476f DONTBUILD

--- toolkit/mozapps/update/tests/unit_aus_update/downloadResumeForSameAppVersion.js
+++ toolkit/mozapps/update/tests/unit_aus_update/downloadResumeForSameAppVersion.js
@@ -5,12 +5,11 @@
 
 function run_test() {
   setupTestCommon();
-  start_httpserver();
 
   debugDump("testing resuming an update download in progress for the same " +
             "version of the application on startup (Bug 485624)");
 
-  let patchProps = {state: STATE_DOWNLOADING, url: gURLData + FILE_SIMPLE_MAR};
+  let patchProps = {state: STATE_DOWNLOADING};
   let patches = getLocalPatchString(patchProps);
   let updateProps = {appVersion: "1.0"};
   let updates = getLocalUpdateString(updateProps, patches);
@@ -36,5 +35,5 @@ function run_test() {
  * Called after the call to waitForUpdateXMLFiles finishes.
  */
 function waitForUpdateXMLFilesFinished() {
-  stop_httpserver(doTestFinish);
+  do_execute_soon(doTestFinish);
 }
