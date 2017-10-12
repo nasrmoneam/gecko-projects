9b5c81c35648162d1fd63f9b481fb229ef608418
Johan Lorenzo <jlorenzo@mozilla.com>
2017-10-12 18:11:29 +0200

    Bug 1407331 - Staging releases: Remove *-dev update channel

--- testing/mozharness/configs/releases/dev_updates_firefox_beta.py
+++ testing/mozharness/configs/releases/dev_updates_firefox_beta.py
@@ -23,15 +23,15 @@ config = {
     "balrog_url": "http://54.90.211.22:9090",
     "balrog_username": "balrog-stage-ffxbld",
     "update_channels": {
-        "beta-dev": {
+        "beta": {
             "version_regex": r"^(\d+\.\d+(b\d+)?)$",
             "requires_mirrors": True,
             # TODO - when we use a real repo, rename this file # s/MozDate/MozBeta-dev/
             "patcher_config": "mozBeta-branch-patcher2.cfg",
-            "update_verify_channel": "beta-dev-localtest",
+            "update_verify_channel": "beta-localtest",
             "mar_channel_ids": [],
-            "channel_names": ["beta-dev", "beta-dev-localtest", "beta-dev-cdntest"],
-            "rules_to_update": ["firefox-beta-dev-cdntest", "firefox-beta-dev-localtest"],
+            "channel_names": ["beta", "beta-localtest", "beta-cdntest"],
+            "rules_to_update": ["firefox-beta-cdntest", "firefox-beta-localtest"],
             "publish_rules": ["firefox-beta"],
             # TEMP stuff
             "bz2_blob_suffix": "-bz2",
--- testing/mozharness/configs/releases/dev_updates_firefox_release.py
+++ testing/mozharness/configs/releases/dev_updates_firefox_release.py
@@ -23,16 +23,16 @@ config = {
     "balrog_url": "http://54.90.211.22:9090",
     "balrog_username": "balrog-stage-ffxbld",
     "update_channels": {
-        "beta-dev": {
+        "beta": {
             "version_regex": r"^(\d+\.\d+(b\d+)?)$",
             "requires_mirrors": False,
             "patcher_config": "mozDate-branch-patcher2.cfg",
-            "update_verify_channel": "beta-dev-localtest",
+            "update_verify_channel": "beta-localtest",
             "mar_channel_ids": [
-                "firefox-mozilla-beta-dev", "firefox-mozilla-release-dev",
+                "firefox-mozilla-beta", "firefox-mozilla-release",
             ],
-            "channel_names": ["beta-dev", "beta-dev-localtest", "beta-dev-cdntest"],
-            "rules_to_update": ["firefox-beta-dev-cdntest", "firefox-beta-dev-localtest"],
+            "channel_names": ["beta", "beta-localtest", "beta-cdntest"],
+            "rules_to_update": ["firefox-beta-cdntest", "firefox-beta-localtest"],
             "publish_rules": ["firefox-beta"],
             "schedule_asap": True,
             # TEMP stuff
@@ -42,14 +42,14 @@ config = {
             "complete_mar_filename_pattern": '%s-%s.bz2.complete.mar',
             "complete_mar_bouncer_product_pattern": '%s-%s-complete-bz2',
         },
-        "release-dev": {
+        "release": {
             "version_regex": r"^\d+\.\d+(\.\d+)?$",
             "requires_mirrors": True,
             "patcher_config": "mozJamun-branch-patcher2.cfg",
-            "update_verify_channel": "release-dev-localtest",
+            "update_verify_channel": "release-localtest",
             "mar_channel_ids": [],
-            "channel_names": ["release-dev", "release-dev-localtest", "release-dev-cdntest"],
-            "rules_to_update": ["firefox-release-dev-cdntest", "firefox-release-dev-localtest"],
+            "channel_names": ["release", "release-localtest", "release-cdntest"],
+            "rules_to_update": ["firefox-release-cdntest", "firefox-release-localtest"],
             "publish_rules": ["firefox-release"],
             # TEMP stuff
             "bz2_blob_suffix": "-bz2",
