e2f51a0af8d72b6c9adc97f343ee03d7c370ee03
Kim Moir <kmoir@mozilla.com>
2017-11-03 12:09:10 -0400

    Bug 1413910 - fix issues during staging release of 57.0 in releng config

--- testing/mozharness/configs/releases/dev_updates_firefox_release.py
+++ testing/mozharness/configs/releases/dev_updates_firefox_release.py
@@ -20,22 +20,22 @@ config = {
     "archive_prefix": "https://ftp.stage.mozaws.net/pub",
     "previous_archive_prefix": "https://archive.mozilla.org/pub",
     "download_domain": "download.mozilla.org",
-    "balrog_url": "http://54.90.211.22:9090",
+    "balrog_url": "https://balrog-admin.stage.mozaws.net/api",
     "balrog_username": "balrog-stage-ffxbld",
     "update_channels": {
-        "beta": {
-            "version_regex": r"^(\d+\.\d+(b\d+)?)$",
-            "requires_mirrors": False,
-            "patcher_config": "mozDate-branch-patcher2.cfg",
-            "update_verify_channel": "beta-localtest",
-            "mar_channel_ids": [
-                "firefox-mozilla-beta", "firefox-mozilla-release",
-            ],
-            "channel_names": ["beta", "beta-localtest", "beta-cdntest"],
-            "rules_to_update": ["firefox-beta-cdntest", "firefox-beta-localtest"],
-            "publish_rules": [32],
-            "schedule_asap": True,
-        },
+#        "beta": {
+#            "version_regex": r"^(\d+\.\d+(b\d+)?)$",
+#            "requires_mirrors": False,
+#            "patcher_config": "mozDate-branch-patcher2.cfg",
+#            "update_verify_channel": "beta-localtest",
+#            "mar_channel_ids": [
+#                "firefox-mozilla-beta", "firefox-mozilla-release",
+#            ],
+#            "channel_names": ["beta", "beta-localtest", "beta-cdntest"],
+#            "rules_to_update": ["firefox-beta-cdntest", "firefox-beta-localtest"],
+#            "publish_rules": [32],
+#            "schedule_asap": True,
+#        },
         "release": {
             "version_regex": r"^\d+\.\d+(\.\d+)?$",
             "requires_mirrors": True,
