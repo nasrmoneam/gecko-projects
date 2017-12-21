03618c81b80f6e91357a6327581a34f960eb72cc
Robert Strong <robert.bugzilla@gmail.com>
2017-12-20 16:55:40 -0800

    Bug 1426487

--- toolkit/mozapps/update/content/updates.js
+++ toolkit/mozapps/update/content/updates.js
@@ -688,7 +688,7 @@ var gUpdatesFoundBasicPage = {
 
 /**
  * The "Update is Downloading" page - provides feedback for the download
- * process plus a pause/resume UI
+ * process
  */
 var gDownloadingPage = {
   /**
@@ -696,12 +696,6 @@ var gDownloadingPage = {
    */
   _downloadStatus: null,
   _downloadProgress: null,
-  _pauseButton: null,
-
-  /**
-   * Whether or not we are currently paused
-   */
-  _paused: false,
 
   /**
    * Label cache to hold the 'Connecting' string
@@ -713,7 +707,6 @@ var gDownloadingPage = {
    */
   _lastSec: Infinity,
   _startTime: null,
-  _pausedStatus: "",
 
   _hiding: false,
 
@@ -728,19 +721,10 @@ var gDownloadingPage = {
   onPageShow() {
     this._downloadStatus = document.getElementById("downloadStatus");
     this._downloadProgress = document.getElementById("downloadProgress");
-    this._pauseButton = document.getElementById("pauseButton");
     this._label_downloadStatus = this._downloadStatus.textContent;
-
-    this._pauseButton.setAttribute("tooltiptext",
-                                   gUpdates.getAUSString("pauseButtonPause"));
-
-    // move focus to the pause/resume button and then disable it (bug #353177)
-    this._pauseButton.focus();
-    this._pauseButton.disabled = true;
 
     var aus = CoC["@mozilla.org/updates/update-service;1"].
               getService(CoI.nsIApplicationUpdateService);
-
     var um = CoC["@mozilla.org/updates/update-manager;1"].
              getService(CoI.nsIUpdateManager);
     var activeUpdate = um.activeUpdate;
@@ -779,24 +763,24 @@ var gDownloadingPage = {
       gUpdates.update.QueryInterface(CoI.nsIWritablePropertyBag);
       gUpdates.update.setProperty("foregroundDownload", "true");
 
-      // Pause any active background download and restart it as a foreground
-      // download.
-      aus.pauseDownload();
-      var state = aus.downloadUpdate(gUpdates.update, false);
-      if (state == "failed") {
-        // We've tried as hard as we could to download a valid update -
-        // we fell back from a partial patch to a complete patch and even
-        // then we couldn't validate. Show a validation error with instructions
-        // on how to manually update.
-        this.cleanUp();
-        gUpdates.wiz.goTo("errors");
-        return;
+      if (gUpdates.update.state != STATE_DOWNLOADING) {
+        var state = aus.downloadUpdate(gUpdates.update, false);
+        if (state == STATE_FAILED) {
+          // We've tried as hard as we could to download a valid update -
+          // we fell back from a partial patch to a complete patch and even
+          // then we couldn't validate. Show a validation error with instructions
+          // on how to manually update.
+          this.cleanUp();
+          gUpdates.wiz.goTo("errors");
+          return;
+        }
       }
       // Add this UI as a listener for active downloads
       aus.addDownloadListener(this);
 
-      if (activeUpdate)
-        this._setUIState(!aus.isDownloading);
+      if (activeUpdate) {
+        this._setUIState();
+      }
     } catch (e) {
       LOG("gDownloadingPage", "onPageShow - error: " + e);
     }
@@ -836,39 +820,18 @@ var gDownloadingPage = {
     [status, this._lastSec] =
       DownloadUtils.getDownloadStatus(aCurr, aMax, rate, this._lastSec);
 
-    // Get the download progress for pausing
-    this._pausedStatus = DownloadUtils.getTransferTotal(aCurr, aMax);
-
     return status;
   },
 
   /**
-   * Adjust UI to suit a certain state of paused-ness
-   * @param   paused
-   *          Whether or not the download is paused
+   * Adjust UI state
    */
-  _setUIState(paused) {
+  _setUIState() {
     var u = gUpdates.update;
-    if (paused) {
-      if (this._downloadProgress.mode != "normal")
-        this._downloadProgress.mode = "normal";
-      this._pauseButton.setAttribute("tooltiptext",
-                                     gUpdates.getAUSString("pauseButtonResume"));
-      this._pauseButton.setAttribute("paused", "true");
-      var p = u.selectedPatch.QueryInterface(CoI.nsIPropertyBag);
-      var status = p.getProperty("status");
-      if (status) {
-        let pausedStatus = gUpdates.getAUSString("downloadPausedStatus", [status]);
-        this._setStatus(pausedStatus);
-      }
-    } else {
-      if (this._downloadProgress.mode != "undetermined")
-        this._downloadProgress.mode = "undetermined";
-      this._pauseButton.setAttribute("paused", "false");
-      this._pauseButton.setAttribute("tooltiptext",
-                                     gUpdates.getAUSString("pauseButtonPause"));
-      this._setStatus(this._label_downloadStatus);
+    if (this._downloadProgress.mode != "undetermined") {
+      this._downloadProgress.mode = "undetermined";
     }
+    this._setStatus(this._label_downloadStatus);
   },
 
   /**
@@ -876,7 +839,6 @@ var gDownloadingPage = {
    */
   _setUpdateApplying() {
     this._downloadProgress.mode = "undetermined";
-    this._pauseButton.hidden = true;
     let applyingStatus = gUpdates.getAUSString("applyingUpdate");
     this._setStatus(applyingStatus);
 
@@ -896,26 +858,6 @@ var gDownloadingPage = {
       Services.obs.removeObserver(this, "update-staged");
       this._updateApplyingObserver = false;
     }
-  },
-
-  /**
-   * When the user clicks the Pause/Resume button
-   */
-  onPause() {
-    var aus = CoC["@mozilla.org/updates/update-service;1"].
-              getService(CoI.nsIApplicationUpdateService);
-    if (this._paused)
-      aus.downloadUpdate(gUpdates.update, false);
-    else {
-      var patch = gUpdates.update.selectedPatch;
-      patch.QueryInterface(CoI.nsIWritablePropertyBag);
-      patch.setProperty("status", this._pausedStatus);
-      aus.pauseDownload();
-    }
-    this._paused = !this._paused;
-
-    // Update the UI
-    this._setUIState(this._paused);
   },
 
   /**
@@ -942,37 +884,9 @@ var gDownloadingPage = {
     // gone away.
     this.cleanUp();
 
-    var aus = CoC["@mozilla.org/updates/update-service;1"].
-              getService(CoI.nsIApplicationUpdateService);
     var um = CoC["@mozilla.org/updates/update-manager;1"].
              getService(CoI.nsIUpdateManager);
     um.activeUpdate = gUpdates.update;
-
-    // If the download was paused by the user, ask the user if they want to
-    // have the update resume in the background.
-    var downloadInBackground = true;
-    if (this._paused) {
-      var title = gUpdates.getAUSString("resumePausedAfterCloseTitle");
-      var message = gUpdates.getAUSString("resumePausedAfterCloseMsg",
-                                          [gUpdates.brandName]);
-      var ps = Services.prompt;
-      var flags = ps.STD_YES_NO_BUTTONS;
-      // Focus the software update wizard before prompting. This will raise
-      // the software update wizard if it is minimized making it more obvious
-      // what the prompt is for and will solve the problem of windows
-      // obscuring the prompt. See bug #350299 for more details.
-      window.focus();
-      var rv = ps.confirmEx(window, title, message, flags, null, null, null,
-                            null, { });
-      if (rv == CoI.nsIPromptService.BUTTON_POS_0)
-        downloadInBackground = false;
-    }
-    if (downloadInBackground) {
-      // Continue download in the background at full speed.
-      LOG("gDownloadingPage", "onHide - continuing download in background " +
-          "at full speed");
-      aus.downloadUpdate(gUpdates.update, false);
-    }
     gUpdates.wiz.cancel();
   },
 
@@ -984,11 +898,6 @@ var gDownloadingPage = {
    *          Additional data
    */
   onStartRequest(request, context) {
-    // This !paused test is necessary because onStartRequest may fire after
-    // the download was paused (for those speedy clickers...)
-    if (this._paused)
-      return;
-
     if (this._downloadProgress.mode != "undetermined")
       this._downloadProgress.mode = "undetermined";
     this._setStatus(this._label_downloadStatus);
@@ -1014,17 +923,10 @@ var gDownloadingPage = {
     p.setProperty("progress", currentProgress);
     p.setProperty("status", status);
 
-    // This !paused test is necessary because onProgress may fire after
-    // the download was paused (for those speedy clickers...)
-    if (this._paused)
-      return;
-
     if (this._downloadProgress.mode != "normal")
       this._downloadProgress.mode = "normal";
     if (this._downloadProgress.value != currentProgress)
       this._downloadProgress.value = currentProgress;
-    if (this._pauseButton.disabled)
-      this._pauseButton.disabled = false;
 
     // If the update has completed downloading and the download status contains
     // the original text return early to avoid an assertion in debug builds.
@@ -1085,7 +987,6 @@ var gDownloadingPage = {
         // Reset the progress meter to "undertermined" mode so that we don't
         // show old progress for the new download of the "complete" patch.
         this._downloadProgress.mode = "undetermined";
-        this._pauseButton.disabled = true;
         document.getElementById("verificationFailed").hidden = false;
         break;
       case CoR.NS_BINDING_ABORTED:
